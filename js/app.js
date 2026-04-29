b1da4115a150da659c01196024a6d7a68754f098
Ahmed Zobayer <ahmed.zobaer98@gmail.com>
2026-04-29 22:42:51 +0600

    Fix quote escaping helper

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@
       .replace(/&/g, "&amp;")
       .replace(/</g, "&lt;")
       .replace(/>/g, "&gt;")
-      .replace(/\"/g, "&quot;")
+      .replace(/"/g, "&quot;")
       .replace(/'/g, "&#39;");
   }
 
